9ad43dee71a9d20654cc29b43b5135ae6746dc0e
Roxana <roxpal@microsoft.com>
2023-01-17 17:17:54 +0000

    Node.js backend

---
+++ my-blog-backend/src/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+
+let articlesInfo = [{
+    name: "learn-react",
+    upvotes: 0,
+    comments: []
+}, {
+    name: "learn-node",
+    upvotes: 0,
+    comments: []
+}, {
+    name: "mongodb",
+    upvotes: 0,
+    comments: []
+}];
+
+const app = express();
+app.use(express.json());
+
+// app.post("/hello", (req, res) => {
+//     console.log(req.body);
+//     res.send(`Hello ${req.body.name}!`); //send back a response
+// });
+
+// app.get("/hello/:name", (req, res) => {
+//     const { name } = req.params;
+//     res.send(`Hello ${name}!!`);
+// });
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(article => article.name === name);
+    
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!!`);
+    } else {
+        res.send("That article doesn\'t exist");
+    }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send("That article doesn\'t exist");
+    }
+})
+
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000")
+})
